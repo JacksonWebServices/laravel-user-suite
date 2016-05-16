88c96b26b8ffd3e9af721ead903f50b337411635
Joshua Chandler <jchandler@jacksonfurnind.com>
2016-05-16 13:00:23 -0400

    Initial commit

---
+++ config/usersuite.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Configurations
+ */
+
+return [];
---
+++ src/UserSuiteServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+namespace JWS\UserSuite;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserSuiteServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->handleConfigs();
+        $this->handleRoutes();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Bind any implementations.
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+
+    private function handleConfigs()
+    {
+        $configPath = __DIR__ . '/../config/usersuite.php';
+
+        $this->publishes([$configPath => config_path('usersuite.php')]);
+
+        $this->mergeConfigFrom($configPath, 'usersuite');
+    }
+
+    private function handleRoutes()
+    {
+        include __DIR__ . '/../routes.php';
+    }
+}
