b61ca9de43fba32a2a51aa7972fd67e2f0513a59
Joshua Chandler <jchandler@jacksonfurnind.com>
2016-05-17 16:00:42 -0400

    A Route access checker for user permissions

--- src/Middleware/RouteAccessChecker.php
+++ src/Middleware/RouteAccessChecker.php
@@ -1,7 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: chandlerj
- * Date: 5/17/2016
- * Time: 3:58 PM
- */
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RouteAccessChecker
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // TODO: Refine this before pushing live
+//        if ($request->user()->hasRole("admin")) {
+//            return $next($request);
+//        }
+
+//        $route = \Request::route()->getName();
+//        if (\Gate::denies($route)) {
+//            return back()
+//                ->with('error_message', "Sorry, page cannot be accessed!");
+//        }
+
+        return $next($request);
+    }
+}
