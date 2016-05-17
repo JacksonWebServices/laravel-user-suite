51b734f8cc8dcfedd7b7fffaf7a308c127db8447
Joshua Chandler <jchandler@jacksonfurnind.com>
2016-05-17 17:45:27 -0400

    Base testing classes

---
+++ tests/AttributeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JWS\UserSuite\Tests;
+
+class AttributeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/PermissionTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JWS\UserSuite\Tests;
+
+class PermissionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
